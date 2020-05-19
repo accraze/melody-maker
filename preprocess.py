dc5e859982c776a6d8e0a74a5826192cbb9991c9
Andy Craze <accraze@gmail.com>
2020-05-19 08:56:03 -0700

    transform songs to time series representations

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import os
 import music21 as m21
 
 KERN_DATASET_PATH = "deutschl/test"
+SAVE_DIR = 'dataset'
 ACCEPTABLE_DURATIONS = [
 	0.25,
 	0.5,
@@ -55,13 +56,35 @@ def transpose(song):
 	transposed_song = song.transpose(interval)
 	return transposed_song
 
+def encode_song(song, time_step=0.25):
+	# p = 60, d = 1.0 -> [60, '_', '_', '_']
+	encoded_song = []
+	for event in song.flat.notesAndRests:
+		# handle notes
+		if isinstance(event, m21.note.Note):
+			symbol = event.pitch.midi
+		elif isinstance(event, m21.note.Rest):
+			symbol = 'r'
+
+		#convert the note/rest into time series notation
+		steps = int(event.duration.quarterLength / time_step)
+		for step in range(steps):
+			if step == 0:
+				encoded_song.append(symbol)
+			else:
+				encoded_song.append('_')
+
+	#cast encoded song to a str
+	encoded_song = ''.join(map(str, encoded_song))
+	return encoded_song
+
 def preprocess(dataset_path):
 	# load folksongs
 	print('loading songs')
 	songs = load_songs_in_kern(dataset_path)
 	print(f'loaded {len(songs)} songs')
 
-	for song in songs:
+	for i, song in enumerate(songs):
 		# filter out songs with non-acceptable durations
 		if not has_acceptable_durations(song, ACCEPTABLE_DURATIONS):
 			continue
@@ -71,11 +94,16 @@ def preprocess(dataset_path):
 		song = transpose(song)
 
 		# encode songs with music time series representation
+		encoded_song = encode_song(song)
 
 		# save songs to text file
+		save_path = os.path.join(SAVE_DIR, str(i))
+		with open(save_path, 'w') as fp:
+			fp.write(encoded_song)
 
 if __name__ == '__main__':
-	songs = load_songs_in_kern(KERN_DATASET_PATH)
-	print(f'loaded {len(songs)} songs')
-	song = songs[0]
-	transpose(song)
+	# songs = load_songs_in_kern(KERN_DATASET_PATH)
+	# print(f'loaded {len(songs)} songs')
+	# song = songs[0]
+	# transpose(song)
+	preprocess(KERN_DATASET_PATH)
